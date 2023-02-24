acc73a80524510a27f7502b2ac83d05cf6a5bf13
maajor <info@ma-yidong.com>
2023-02-24 23:36:53 +0800

    update tester

--- train/test_lightmap.py
+++ train/test_lightmap.py
@@ -22,23 +22,34 @@ def load_exr(path: str, channels=("R", "G", "B")):
 
 
 def get_uv():
-    uv = load_exr("lightmap/vuv0.exr", ("ViewLayer.UV.U", "ViewLayer.UV.V"))
-    return uv
+    uv = load_exr("dataset_raw/uv/Camera.002.exr", ("View Layer.UV.U", "View Layer.UV.V"))
+    uv = np.swapaxes(uv,0,1)
+    w, h, c = uv.shape
+    ones = np.ones((w,h,2))
+    uvimg = np.concatenate([uv, ones], -1)
+    img_uv = Image.fromarray((uvimg * 255.0).astype(np.uint8))
+    img_uv.save(f"image_uv.png")
+    return uv[0:-1:4, 0:-1:4, :]
 
 
 def get_groundtruth_pn():
-    dataset = np.load("dataset/render_2k.npy", allow_pickle=True)
-    pn = dataset.item().get("pn0")[:, :, :]
-    img_pil = Image.fromarray((pn[:, :, 0:3] * 255.0).astype(np.uint8))
+    dataset = np.load("dataset/render_text_4k.npz", allow_pickle=True)
+    pn = dataset['pn0'][0:-1:4, 0:-1:4, :]
+    normalized = pn * 0.5 + 0.5
+    img_pil = Image.fromarray((normalized[:, :, 0:3] * 255.0).astype(np.uint8))
     img_pil.save(f"groundtruth_p.png")
-    img_pil = Image.fromarray((pn[:, :, 3:6] * 255.0).astype(np.uint8))
+    img_pil = Image.fromarray((normalized[:, :, 3:6] * 255.0).astype(np.uint8))
     img_pil.save(f"groundtruth_n.png")
     return pn
 
 
 def get_groundtruth_v():
-    dataset = np.load("dataset/render_2k.npy", allow_pickle=True)
-    v = dataset.item().get("v0")[:, :, :]
+    dataset = np.load("dataset/render_text_4k.npz", allow_pickle=True)
+    v = dataset["v0"][0:-1:4, 0:-1:4, :]
+    print(np.max(v))
+    print(np.min(v))
+    img_pil = Image.fromarray((v[:, :, 0:3] * 255.0).astype(np.uint8))
+    img_pil.save(f"groundtruth_v.png")
     return v
 
 
@@ -54,6 +65,8 @@ def bilinear_sample_texture(u0, v0, u1, v1, ru, rv, texture):
 def get_lightmap_pn():
     position = load_exr("lightmap/position.exr") * 2.0 - 1.0
     normal = load_exr("lightmap/normal.exr") * 2.0 - 1.0
+    position = np.swapaxes(position,0,1)
+    normal = np.swapaxes(normal,0,1)
     return np.concatenate((position, normal), -1)
 
 
@@ -61,18 +74,23 @@ def get_lightmap_pn_view():
     uv = get_uv()
     position = load_exr("lightmap/position.exr") * 2.0 - 1.0
     normal = load_exr("lightmap/normal.exr") * 2.0 - 1.0
+    position = np.swapaxes(position,0,1)
+    normal = np.swapaxes(normal,0,1)
     lmw, lmh, _ = position.shape
     w, h, c = uv.shape
     npview = np.zeros((w, h, 6))
+    print('start get lightmap pn view')
     for i in range(w):
+        if i % 100 == 0:
+            print(i)    
         for j in range(h):
             uvpix, _ = np.modf(uv[i, j])
-            u_0 = min(math.floor(uvpix[0] * lmw), lmw - 1)
-            v_0 = min(math.floor((1 - uvpix[1]) * lmh), lmh - 1)
+            u_0 = min(math.floor((1-uvpix[1]) * lmw), lmw - 1)
+            v_0 = min(math.floor((uvpix[0]) * lmh), lmh - 1)
             u_1 = min(u_0 + 1, lmw - 1)
             v_1 = min(v_0 + 1, lmh - 1)
-            r_u = uvpix[0] * lmw - u_0
-            r_v = (1 - uvpix[1]) * lmh - v_0
+            r_u = (1-uvpix[1]) * lmw - u_0
+            r_v = (uvpix[0]) * lmh - v_0
             npview[i, j, 0:3] = bilinear_sample_texture(
                 u_0, v_0, u_1, v_1, r_u, r_v, position
             )
@@ -93,6 +111,7 @@ def predict_with_gt_pn():
     v = get_groundtruth_v()
 
     model.eval()
+    print('predict_with_gt_pn')
     with torch.no_grad():
         pn = torch.from_numpy(pn).float()
         v = torch.from_numpy(v).float()
@@ -107,6 +126,7 @@ def predict_with_lightmap_pn():
     v = get_groundtruth_v()
 
     model.eval()
+    print('predict_with_lightmap_pn')
     with torch.no_grad():
         pn = torch.from_numpy(pn).float()
         v = torch.from_numpy(v).float()
@@ -149,6 +169,20 @@ def bake_lightmap():
         img_pil.save(f"lightmap_{i/4}.png")
 
 
+def load_lightmap():
+    channs = []
+    for i in range(4):
+        img = Image.open(f'lightmap_{i}.0.png')
+        channs.append(np.array(img))
+    return np.concatenate(channs, -1)
+
+def assert_lms():
+    load_lms = load_lightmap()
+    lms_from_uv = get_lightmap_from_uv() * 0.5 + 0.5
+    lms_from_uv = (lms_from_uv * 255.0).astype(np.uint8)
+    print(np.max(load_lms - lms_from_uv))
+
+
 def inference_with_lightmap():
     lightmap = get_lightmap_from_view()
     view = get_groundtruth_v()
@@ -183,8 +217,42 @@ def debug_lightmap():
         # print(np.sin((np.matmul(w, v) + b)*a))
 
 
+def predict_from_datasource(name: str):
+    pn_image = load_exr(
+        f"dataset_raw/pn/{name}.exr",
+        channels=(
+            "View Layer.Position.X",
+            "View Layer.Position.Y",
+            "View Layer.Position.Z",
+            "View Layer.Normal.X",
+            "View Layer.Normal.Y",
+            "View Layer.Normal.Z",
+        ),
+    )
+    pn_image = np.swapaxes(pn_image,0,1)[0:-1:2, 0:-1:2, :]
+
+    view_img = load_exr(str(f"dataset_raw/viewold/{name}.exr"), channels=("R", "G", "B"))
+    view_img = np.swapaxes(view_img,0,1)[0:-1:2, 0:-1:2, :]
+
+    model.eval()
+    with torch.no_grad():
+        pn = torch.from_numpy(pn_image).float()
+        v = torch.from_numpy(view_img).float()
+        render = model(pn, v)
+        render = render.numpy()
+
+        img_pil = Image.fromarray((render * 255.0).astype(np.uint8))
+        img_pil.save(f"render_{name}.png")
+
+
+
+
 if __name__ == "__main__":
-    bake_lightmap()
+    # bake_lightmap()
+    # predict_with_gt_pn()
+    # predict_with_lightmap_pn()
     # inference_with_lightmap()
+    # get_uv()
     # predict_with_gt_pn()
     # debug_lightmap()
+    predict_from_datasource('Camera.028')
