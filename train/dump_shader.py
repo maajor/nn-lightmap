c5b5ea2b18efd36fdf97b9e1bc7a1f63aca2613c
maajor <info@ma-yidong.com>
2023-02-28 20:33:06 +0800

    update model

--- train/dump_shader.py
+++ train/dump_shader.py
@@ -2,6 +2,6 @@ from model import SirenGINet
 import torch
 
 if __name__ == '__main__':
-    model = SirenGINet(256, 5, 8, 64, 2)
-    model.load_state_dict(torch.load("model/model_siren_256x5x8x64x2_2000.pth"))
+    model = SirenGINet(256, 5, 8, 32, 2)
+    model.load_state_dict(torch.load("model/model_siren_256x5x8x32x2_2200.pth"))
     model.dump_shader()
--- train/test_lightmap.py
+++ train/test_lightmap.py
@@ -7,8 +7,8 @@ import array
 from PIL import Image
 from model import SirenGINet
 
-model = SirenGINet(256, 5, 8, 64, 2)
-model.load_state_dict(torch.load("model/model_siren_256x5x8x64x2_2000.pth"))
+model = SirenGINet(256, 5, 8, 32, 2)
+model.load_state_dict(torch.load("model/model_siren_256x5x8x32x2_2200.pth"))
 
 
 def load_exr(path: str, channels=("R", "G", "B")):
